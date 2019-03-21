1a9d0428fb8700a01d959f194a9dd0065e2a1a2c
08822407d <08822407d@gmail.com>
2019-03-21 21:53:22 +0800

    增加了部分功能键处理

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -9,6 +9,19 @@
 
 PRIVATE struct kbd_s    kbd_in;
 
+PRIVATE int     code_with_E0 = 0;
+PRIVATE int     shift_l;
+PRIVATE int     shift_r;
+PRIVATE int     alt_l;
+PRIVATE int     alt_r;
+PRIVATE int     ctrl_l;
+PRIVATE int     ctrl_r;
+PRIVATE int     caps_lock;
+PRIVATE int     num_lock;
+PRIVATE int     scroll_lock;
+
+PRIVATE int     column;
+
 void keyborad_handler()
 {
     u8_t scan_code = in_b(KBD_IO);
@@ -28,6 +41,8 @@ PUBLIC void read_kbdbuff()
     u8_t    scan_code;
     char    output[2];
     u32_t   make;
+    u32_t   key = 0;
+    u32_t  *keyrow;
 
     if (kbd_in.count > 0)
     {
@@ -38,8 +53,6 @@ PUBLIC void read_kbdbuff()
         kbd_in.p_tail = ((u32_t)(kbd_in.p_tail) & KBDUFF_SIZE) +
                         kbd_in.buf;
         kbd_in.count--;
-
-//        enable_intr();
 
         if (scan_code == 0xE1)
         {
@@ -52,13 +65,63 @@ PUBLIC void read_kbdbuff()
         else
         {
             make = (scan_code & FLAG_BREAK ? FALSE : TRUE);
-            if (make)
+
+            keyrow = &keymap[(scan_code&0x7F)*MAP_COLS];
+
+            column = 0;
+            if (shift_l || shift_r)
             {
-                output[0] = keymap[(scan_code&0x7F)*MAP_COLS];
+                column = 1;
+            }
+            if (code_with_E0)
+            {
+                column = 2;
+                code_with_E0 = 0;
+            }
+            key = keyrow[column];
+
+            switch (key)
+            {
+                case SHIFT_L :
+                    shift_l = make;
+                    key = 0;
+                    break;
+                case SHIFT_R :
+                    shift_r = make;
+                    key = 0;
+                    break;
+                case CTRL_L :
+                    ctrl_l = make;
+                    key = 0;
+                    break;
+                case CTRL_R :
+                    ctrl_r = make;
+                    key = 0;
+                    break;
+                case ALT_L :
+                    alt_l = make;
+                    key = 0;
+                    break;
+                case ALT_R :
+                    alt_r = make;
+                    key = 0;
+                    break;
+                default :
+                    if (!make)
+                    {
+                        key = 0;
+                    }
+                    break;
+            }
+
+            if (key)
+            {
+                output[0] = key;
                 disp_str(output);
             }
         }
-        
+    
+//        enable_intr();
     }
 }
 

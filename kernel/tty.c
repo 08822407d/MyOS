36d9565abfda7217d7a2cdb48ac5f5d0df720957
08822407d <08822407d@gmail.com>
2019-04-14 22:21:09 +0800

    暂存

--- kernel/tty.c
+++ kernel/tty.c
@@ -64,40 +64,45 @@ bool is_current_console(CONSOLE_t* console_ptr)
     return (console_ptr == &console_table[current_console_idx]);
 }
 
-PUBLIC void in_process(TTY_t* tty_ptr, u32_t key)
+PUBLIC void in_process(TTY_t *tty_ptr, u32_t key)
 {
-        char output[2] = {'\0', '\0'};
+    char output[2] = {'\0', '\0'};
 
-        if (!(key & FLAG_EXT)) {
-		if (tty_ptr->inbuff_count < TTY_IN_BUF_SIZE) {
-			*(tty_ptr->inbuff_head_ptr) = key;
-			tty_ptr->inbuff_head_ptr++;
-			if (tty_ptr->inbuff_head_ptr == tty_ptr->inbuff + TTY_IN_BUF_SIZE) {
-				tty_ptr->inbuff_head_ptr = tty_ptr->inbuff;
-			}
-			tty_ptr->inbuff_count++;
-		}
+    if (!(key & FLAG_EXT))
+    {
+        if (tty_ptr->inbuff_count < TTY_IN_BUF_SIZE)
+        {
+            *(tty_ptr->inbuff_head_ptr) = key;
+            tty_ptr->inbuff_head_ptr++;
+            tty_ptr->inbuff_head_ptr == ((u32_t)tty_ptr->inbuff_ptr & (TTY_IN_BUF_SIZE - 1) +
+                                        tty_ptr->inbuff;
+            tty_ptr->inbuff_count++;
         }
-        else {
-                int raw_code = key & MASK_RAW;
-                switch(raw_code) {
-                case UP:
-                        if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
-                                //disable_int();
-                                out_b(CRTC_ADDR_REG, START_ADDR_H);
-                                out_b(CRTC_DATA_REG, ((80*15) >> 8) & 0xFF);
-                                out_b(CRTC_ADDR_REG, START_ADDR_L);
-                                out_b(CRTC_DATA_REG, (80*15) & 0xFF);
-                                //enable_int();
-                        }
-                        break;
-                case DOWN:
-                        if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R)) {
-				/* Shift+Down, do nothing */
-                        }
-                        break;
-                default:
-                        break;
-                }
+    }
+    else
+    {
+        int raw_code = key & MASK_RAW;
+        switch (raw_code)
+        {
+        case UP:
+            if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
+            {
+                //disable_int();
+                out_b(CRTC_ADDR_REG, START_ADDR_H);
+                out_b(CRTC_DATA_REG, ((80 * 15) >> 8) & 0xFF);
+                out_b(CRTC_ADDR_REG, START_ADDR_L);
+                out_b(CRTC_DATA_REG, (80 * 15) & 0xFF);
+                //enable_int();
+            }
+            break;
+        case DOWN:
+            if ((key & FLAG_SHIFT_L) || (key & FLAG_SHIFT_R))
+            {
+                /* Shift+Down, do nothing */
+            }
+            break;
+        default:
+            break;
         }
+    }
 }
